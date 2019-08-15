4b9952671da19d54a787bfb71c9f19a0b692ebf8
ivanajuliao <ivana_juliao@yahoo.com.br>
2019-08-15 08:59:01 -0300

    Criação do projeto

---
+++ SportsEvent/src/sportsevent/SportsEvent.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sportsevent;
+
+/**
+ *
+ * @author 20131BSI0173
+ */
+public class SportsEvent {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
