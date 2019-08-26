9108fca81c916298fcbd697dcbf8fe96f43b5971
ivanajuliao <ivana_juliao@yahoo.com.br>
2019-08-26 10:41:33 -0300

    Exercício do padrão Builder

---
+++ BuilderRefeicaoLanchonete/src/refeicao/RefeicaoBuilder.java
@@ -0,0 +1,24 @@
+
+
+package refeicao;
+
+/**
+ *
+ * @author felipe
+ */
+public abstract class RefeicaoBuilder {    
+    Refeicao refeicao;        
+    
+    public void adicionarSuco(String sabor){                                
+        refeicao.setBebida(new Suco(sabor));        
+    }   
+       
+    public void adicionarRefrigerante(String sabor){
+        refeicao.setBebida(new Refrigerante(sabor));        
+    } 
+    
+    public void preparaBatata(Batata batata){
+        refeicao.setBatata(batata);
+    }
+    
+}
